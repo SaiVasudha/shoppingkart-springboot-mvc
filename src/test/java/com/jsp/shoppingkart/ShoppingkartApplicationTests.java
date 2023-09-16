7d565869046cbb5dabbaf04834e320cf3c6f566e
saivasudha <sai.vasudha@codecraft.co.in>
2023-09-16 12:13:44 +0530

    first commit

---
+++ src/test/java/com/jsp/shoppingkart/ShoppingkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.shoppingkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
